79ba8628e448450a40a358d2508f73fa8123b64d
Zaynab <68663353+ZaynabMohammed@users.noreply.github.com>
2025-07-19 22:27:11 +0300

    Update Pub_ALB-SG.tf

--- Pub_ALB_Module/Pub_ALB-SG.tf
+++ Pub_ALB_Module/Pub_ALB-SG.tf
@@ -8,6 +8,13 @@ resource "aws_security_group" "alb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+ ##_for_Proxy_Listener_## 
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
